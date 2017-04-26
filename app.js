1ad96b2f512a28282a1921e41a7f8d2a05f48710
sajithaliyanage <sajithaliyanage@gmail.com>
2017-04-26 11:06:19 +0530

    updated

--- app.js
+++ app.js
@@ -57,8 +57,8 @@ app.listen(process.env.PORT || 3000, function(){
 });
 
 //connection og MongoDB
-var conn = mongoose.connect('localhost:27017/travel');
-//var conn = mongoose.connect('mongodb://sajithaliyanage:capn@sv12@ds161950.mlab.com:61950/travel');
+//var conn = mongoose.connect('localhost:27017/travel');
+var conn = mongoose.connect('mongodb://sajithaliyanage:capn@sv12@ds161950.mlab.com:61950/travel');
 if(conn){
   console.log('MongoDB Connected');
 }else{
